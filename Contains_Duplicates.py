7386f820915020ec752479436b181eeb3b712785
Yash <nema.yash003@gmail.com>
2022-05-20 15:03:21 -0400

    Update Contains_Duplicates.py

--- Contains_Duplicates.py
+++ Contains_Duplicates.py
@@ -6,3 +6,13 @@ class Solution:
             return True
         else:
             return False
+
+#Test Cases        
+#Input: nums = [1,2,3,1]
+#Output: true
+
+#Input: nums = [1,2,3,4]
+#Output: false
+
+#Input: nums = [1,1,1,3,3,4,3,2,4,2]
+#Output: true
