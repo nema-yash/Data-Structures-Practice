9093bc2fcf2bdb3c220712058915830b9c82e1eb
Yash <nema.yash003@gmail.com>
2022-05-29 15:26:51 -0400

    Add files via upload
    Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.

---
+++ Intersection_of_Two_Arrays_II.py
@@ -0,0 +1,17 @@
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        result=[]
+        for i in nums1:
+            if i in nums2:
+                result.append(i)
+                nums2.remove(i)
+        return result
+
+
+#Input: nums1 = [1,2,2,1], nums2 = [2,2]
+#Output: [2,2]
+
+#Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+#Output: [4,9]
+#Explanation: [9,4] is also accepted.
+
